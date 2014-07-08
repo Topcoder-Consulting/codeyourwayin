5bdbbbef65ec0856f9bfbd702d3589ddd2d51d5a
Jeff Douglas <jeff@jeffdouglas.com>
2014-07-08 11:07:53 -0400

    added tests for heroku

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,12 @@
  * Home page.
  */
 
+ var verification = require("../lib/verification.js");
+
 exports.index = function(req, res) {
+
+verification.login(req.cookies.tcsso);
+
   res.render('home', {
     title: 'Home'
   });
